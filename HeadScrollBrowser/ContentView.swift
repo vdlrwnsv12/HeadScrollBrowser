558fe40144e1c094c845e0b891b67c3efd0b6173
vdlrwnsv12 <vdlrwnsv12@gmail.com>
2026-01-21 16:56:48 +0900

    Initial Commit

---
+++ HeadScrollBrowser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeadScrollBrowser
+//
+//  Created by 윤익준의 스타벅스 자유이용권 on 1/21/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeadScrollBrowser/HeadScrollBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeadScrollBrowserApp.swift
+//  HeadScrollBrowser
+//
+//  Created by 윤익준의 스타벅스 자유이용권 on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct HeadScrollBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
